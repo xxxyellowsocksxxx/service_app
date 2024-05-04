ef704e3704406a5c3f86f3fad55b2271eab65422
Pro100JlUDiN <pro100jludin@gmail.com>
2024-05-04 22:05:01 +0200

    second

--- service/services/views.py
+++ service/services/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Prefetch, F, Avg
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from services.models import Subscription
+from clients.models import Client
 from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.all().prefetch_related('plan',
+                                                           'client',
+                                                           'client__user').only('client__user__email',
+                                                                                'client__company_name',
+                                                                                'plan_id')
+
     serializer_class = SubscriptionSerializer
